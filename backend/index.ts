c064d42106f325e2f4e745717a469c83605317a2
DarkFlarex <darkflarexxx@gmail.com>
2024-10-01 13:20:04 +0600

    В index реализована обработка сообщений о рисовании и передача существующих пикселей подключенным клиентам.

--- backend/index.ts
+++ backend/index.ts
@@ -3,6 +3,16 @@ import expressWs from 'express-ws';
 import cors from 'cors';
 import { WebSocket} from 'ws';
 
+interface DrawPoint {
+    offsetX: number;
+    offsetY: number;
+}
+
+interface IncomingDraw {
+    type: string;
+    payload: DrawPoint[];
+}
+
 const app = express();
 expressWs(app);
 app.use(cors());
@@ -11,10 +21,30 @@ const router = express.Router();
 
 const connectedClients: WebSocket[] = [];
 
+let filledPixels: DrawPoint[] = [];
+
 router.ws('/canvas', (ws, req) => {
     connectedClients.push(ws);
     console.log('client connected, total clients: ', connectedClients.length);
 
+    ws.send(JSON.stringify({ type: 'EXISTING_PIXEL', payload: filledPixels }));
+
+    ws.on('message', (drawMessage) => {
+        try {
+            const decodedDraw = JSON.parse(drawMessage.toString()) as IncomingDraw;
+            if (decodedDraw.type === 'EXISTING_PIXEL') {
+                filledPixels.push(...decodedDraw.payload);
+                connectedClients.forEach((clientWs) => {
+                    clientWs.send(JSON.stringify({
+                        type: 'NEW_PIXEL',
+                        payload: decodedDraw.payload,
+                    }));
+                });
+            }
+        } catch (e) {
+            ws.send(JSON.stringify({error: 'Invalid Draw'}));
+        }
+    });
 
     ws.on('close', ()=>{
         console.log('client disconnected');
